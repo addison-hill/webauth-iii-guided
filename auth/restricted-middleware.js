fd7b8f69bf012156d0b12f99f289ee231938b207
Addison Hill <adddison.hill@gmail.com>
2019-12-18 13:06:12 -0600

    added JWT support and middlware checking for a token

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,21 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const Users = require('../users/users-model.js');
+// const Users = require("../users/users-model.js");
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  const { authorization } = req.headers;
+  if (authorization) {
+    const secret = "is it secret, is it safe?";
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "Invalid token." });
+      } else {
+        req.token = decodedToken;
+        next();
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "Please login and try again." });
   }
 };
